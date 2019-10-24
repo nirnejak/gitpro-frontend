f4c1ab470bef030f1bc9e6ffbb164bfb57291790
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-10-24 17:53:11 +0530

    Footer Links Position, Logo in Footer

--- src/components/UI/Footer.vue
+++ src/components/UI/Footer.vue
@@ -3,48 +3,53 @@
     <div class="container">
       <div class="row py-70 mt-30">
         <div class="col-6">
-          <div class="text-center py-30">
+          <div class="text-center">
             <router-link to="/" class="text-high-contrast">
-              <i class="fab fa-github fa-4x" />
-              <h3>GitPro</h3>
+              <img src="@/assets/logo.svg" alt="GitPro Logo" width="30%" />
+              <h3 class="m-0">GitPro</h3>
             </router-link>
           </div>
         </div>
-        <div class="col-3">
+        <div class="col-3 is-vertical-align">
           <div>
-            <strong>Quick Links</strong>
-          </div>
-          <br />
-          <div>
-            <router-link to="/faq" class="text-dark">FAQ</router-link>
-          </div>
-          <br />
-          <div>
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSe24lZ1ykY24mTfog59DlbOOFtrTTeaYoT0PNQ3cxmvl_o42w/viewform"
-              class="text-dark"
-            >Submit Feedback</a>
-          </div>
-          <br />
-          <div>
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSe24lZ1ykY24mTfog59DlbOOFtrTTeaYoT0PNQ3cxmvl_o42w/viewform"
-              class="text-dark"
-            >Request a Feature</a>
+            <div>
+              <strong>Quick Links</strong>
+            </div>
+            <br />
+            <div>
+              <router-link to="/faq" class="text-dark">FAQ</router-link>
+            </div>
+            <br />
+            <div>
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSe24lZ1ykY24mTfog59DlbOOFtrTTeaYoT0PNQ3cxmvl_o42w/viewform"
+                class="text-dark"
+              >Submit Feedback</a>
+            </div>
+            <br />
+            <div>
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSe24lZ1ykY24mTfog59DlbOOFtrTTeaYoT0PNQ3cxmvl_o42w/viewform"
+                class="text-dark"
+              >Request a Feature</a>
+            </div>
           </div>
         </div>
-        <div class="col-3">
+        <div class="col-3 is-vertical-align">
           <div>
-            <strong>Contact</strong>
+            <div>
+              <strong>Contact</strong>
+            </div>
+            <br />
+            <div>products@inkoop.io</div>
+            <br />
+            <div class="text-dark">
+              Developed by
+              <a href="https://inkoop.io" target="_blank">Inkoop.io</a>
+            </div>
+            <br />
+            <div>&nbsp;</div>
           </div>
-          <br />
-          <div>products@inkoop.io</div>
-          <br />
-          <div class="text-dark">
-            Developed by
-            <a href="https://inkoop.io" target="_blank">Inkoop.io</a>
-          </div>
-          <br />
         </div>
       </div>
     </div>
