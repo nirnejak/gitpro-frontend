a4f2ef6bd7369f11250ecb6f8f0a5bb8a5d14672
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-11-02 17:00:24 +0530

    Login Page: Layout and Info Added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,42 @@
 <template>
   <div class="hero-container">
-    <div class="container h-100vh">
-      <div class="row">
-        <h1>Login</h1>
+    <div class="row">
+      <div class="col-7-md">
+        <div class="is-vertical-align h-100vh">
+          <h1 class="text-center">Secure Login</h1>
+        </div>
+      </div>
+      <div class="col-5-md bg-card offset-1">
+        <div class="is-vertical-align is-center h-100vh">
+          <div class="text-center">
+            <h1>Login/SignUp</h1>
+            <button class="button dark continue-github">
+              <i class="fab fa-sm fa-github" />
+              Continue with GitHub
+            </button>
 
-        <i class="fab fa-sm fa-github" />
+            <div>
+              <div class="mt-70 text-dark">
+                <p class="text-left">
+                  <i class="fas fa-user-lock mr-5" />
+                  Secure login with GitHub
+                </p>
+                <p class="text-left">
+                  <i class="fas fa-shield-alt mr-10" />
+                  All your data is secure
+                </p>
+                <p class="text-left">
+                  <i class="fas fa-user-shield mr-5" />
+                  User privacy, no tracking
+                </p>
+                <p class="text-left">
+                  <i class="fas fa-trash mr-10" />
+                  Deleted after processing
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -30,4 +62,7 @@ export default {
 </script>
 
 <style>
+.button.continue-github {
+  transform: scale(1.2);
+}
 </style>
