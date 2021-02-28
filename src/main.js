8a05bee972d8bf6102f442385c75d99883e023f2
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-02-28 19:27:37 +0530

    Dependencies Updated

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ import * as Integrations from '@sentry/integrations'
 
 import Message from 'vue-m-message'
 
-import 'circular-std'
 import 'vue-multiselect/dist/vue-multiselect.min.css'
 
 Sentry.init({
@@ -43,5 +42,5 @@ new Vue({
   router,
   store,
   render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
+  mounted: () => document.dispatchEvent(new Event('x-app-rendered'))
 }).$mount('#app')
